45aad402324c29bc4f7131802c023b4dba21d19a
Yazmin Perez <zoeperez333@gmail.com>
2022-03-11 12:09:32 -0800

    add test for mercury class (red phase)

---
+++ __test__/mercury.test.js
@@ -0,0 +1,12 @@
+import Mercury from './../src/mercury.js';
+
+describe ('Mercury', () => {
+
+  test('should return mercury function', () => {
+    let mercury = new Mercury("Z");
+    expect(mercury.name).toEqual("");
+    expect(mercury.earthAge).toEqual(0);
+    expect(mercury.lifeExpectancy).toEqual(80);
+    expect(mercury.mercurySolar).toEqual(1.88);
+  });
+});
