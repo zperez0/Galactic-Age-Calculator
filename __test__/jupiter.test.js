8bf5e40be6d5383fe80d3570b8cd8a25b591b508
Yazmin Perez <zoeperez333@gmail.com>
2022-03-10 16:37:12 -0800

    remove jupiterSolarYear method test. add/update new test to jupiterAge (red phase)

--- __test__/jupiter.test.js
+++ __test__/jupiter.test.js
@@ -5,27 +5,16 @@ describe ('jupiter', () => {
   test('should return jupiter function',() => {
     let jupiter = new Jupiter("Z");
     expect(jupiter.name).toEqual("");
-    expect(jupiter.age).toEqual(0);
+    expect(jupiter.earthAge).toEqual(0);
     expect(jupiter.lifeExpectancy).toEqual(0);
   });
 });
 
-// describe ('jupiterSolarYear', () => {
-
-//   test('should convert earth solar year to jupiter solar year', () => {
-//     let jupiter = new Jupiter();
-//     let earthSolar = 365;
-//     let jupiterSolar = 12;
-//     expect(jupiter.jupiterSolarYear(earthSolar, jupiterSolar)).toEqual(4380);
-//   });
-// });
-
 describe ('jupiterAge', () => {
 
-  test('should convert human earth age to jupiter age', () => {
+  test('should convert human earth age to earth days', () => {
     let jupiter = new Jupiter();
-    let jupiterDays = 4380;
-    let earthAge = 2;
-    expect(jupiter.jupiterAge(jupiterDays, earthAge)).toEqual(24);
+    jupiter.earthAge = 2;
+    expect(jupiter.jupiterAge()).toEqual(730);
   });
 });
--- src/jupiter.js
+++ src/jupiter.js
@@ -1,17 +1,7 @@
 export default class Jupiter {
   constructor() {
   this.name = "";
-  this.age = 0;
+  this.earthAge = 0;
   this.lifeExpectancy = 0;
-  this.earthSolar = 365;
-  }
-
-  jupiterSolarYear(earthSolar, jupiterSolar) {
-    return earthSolar * jupiterSolar;
-  }
-
-  jupiterAge(jupiterDays, earthAge) {
-    let jupAge = jupiterDays * earthAge;
-    return jupAge / this.earthSolar;
   }
 }
