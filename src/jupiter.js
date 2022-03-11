f6cabd491e69c333aac1033a3d26e1da0ef0c390
Yazmin Perez <zoeperez333@gmail.com>
2022-03-11 09:31:46 -0800

    add earthConverter to jupiter.js. Test passing

--- src/jupiter.js
+++ src/jupiter.js
@@ -5,4 +5,9 @@ export default class Jupiter {
   this.lifeExpectancy = 80;
   this.jupiterSolar = 12;
   }
+
+  earthConverter() {
+    let earthDays = this.earthAge * 365;
+    return earthDays;
+  }
 }
