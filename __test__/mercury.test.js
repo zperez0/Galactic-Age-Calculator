151aa3d02ab8b5ce4d25cc639d5255284063a19f
Yazmin Perez <zoeperez333@gmail.com>
2022-03-11 12:32:33 -0800

    add earthConverter test for mercury (red phase)

--- __test__/mercury.test.js
+++ __test__/mercury.test.js
@@ -3,10 +3,19 @@ import Mercury from './../src/mercury.js';
 describe ('Mercury', () => {
 
   test('should return mercury function', () => {
-    let mercury = new Mercury("Z");
+    let mercury = new Mercury();
     expect(mercury.name).toEqual("");
     expect(mercury.earthAge).toEqual(0);
     expect(mercury.lifeExpectancy).toEqual(80);
     expect(mercury.mercurySolar).toEqual(.24);
+  });
+});
+
+describe('earthConverter', () => {
+
+  test('should convert human earth age to earth days', () => {
+    let mercury = new Mercury();
+    mercury.earthAge = 2; 
+    expect(mercury.earthConverter()).toEqual(730);
   });
 });
